ee36bdae7553dd1674e86017b3c827bd184fd6a7
Alexandra S <alexandrastanjic@gmail.com>
2021-06-21 13:12:47 +0300

    let users logout

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,14 +3,20 @@ const User = require('../models/User');
 exports.login = function(req, res) {
 	let user = new User(req.body);
 	user.login().then(function(result){
-		req.session.user = {favColor: "blue", username: user.data.username}
-		res.send(result);
+		req.session.user = {favColor: "blue", username: user.data.username};
+		req.session.save(function(){
+			res.redirect('/');
+		})
 	}).catch(function(err){
 			res.send(err);		
 	});
 };
 
-exports.logout = function(req, res) {};
+exports.logout = function(req, res) {
+	req.session.destroy(function(){
+		res.redirect('/');
+	})
+};
 
 exports.register = function(req, res) {
 	let user = new User(req.body);
