1aee6d94a11fc5cf2769ee3228d2120e8a32fb14
Alexandra S <alexandrastanjic@gmail.com>
2021-06-26 07:21:52 +0300

    add user registration improvements

--- models/User.js
+++ models/User.js
@@ -1,4 +1,4 @@
-const bcrypt = require("bcryptjs")
+const bcrypt = require('bcryptjs');
 const usersCollection = require('../db').db().collection('users');
 const validator = require('validator');
 
@@ -22,64 +22,97 @@ User.prototype.cleanUp = function() {
 		username: this.data.username.trim().toLowerCase(),
 		email: this.data.email.trim().toLowerCase(),
 		password: this.data.password
-	}
+	};
 };
 
 User.prototype.validate = function() {
-	if (this.data.username == '') {
-		this.errors.push('You must provide a username');
-	}
-	if (this.data.username != '' && !validator.isAlphanumeric(this.data.username)) {
-		this.errors.push('Username can only contain letters and numbers');
-	}
-	if (!validator.isEmail(this.data.email)) {
-		this.errors.push('You must provide email');
-	};
-	if (this.data.password == '') {
-     this.errors.push('You must provide a password');
-	}
-	if (this.data.password.length > 0 && this.data.password.length < 12) {
-		this.errors.push('Your password must be at least 12 characters');
-	}
-	if (this.data.password.length > 50) {
-		this.errors.push('Password cannot exceed 50 characters');
-	}
-	if (this.data.username.length > 0 && this.data.username.length < 3) {
-		this.errors.push('Username must be at least 3 characters');
-	}
-	if (this.data.username.length > 30) {
-		this.errors.push('Username  cannot exceed 100 characters');
-	}
+	return new Promise(async (resolve, reject) => {
+		if (this.data.username == '') {
+			this.errors.push('You must provide a username');
+		}
+		if (this.data.username != '' && !validator.isAlphanumeric(this.data.username)) {
+			this.errors.push('Username can only contain letters and numbers');
+		}
+		if (!validator.isEmail(this.data.email)) {
+			this.errors.push('You must provide email');
+		}
+		if (this.data.password == '') {
+			this.errors.push('You must provide a password');
+		}
+		if (this.data.password.length > 0 && this.data.password.length < 12) {
+			this.errors.push('Your password must be at least 12 characters');
+		}
+		if (this.data.password.length > 50) {
+			this.errors.push('Password cannot exceed 50 characters');
+		}
+		if (this.data.username.length > 0 && this.data.username.length < 3) {
+			this.errors.push('Username must be at least 3 characters');
+		}
+		if (this.data.username.length > 30) {
+			this.errors.push('Username  cannot exceed 100 characters');
+		}
+
+		// Only if username is valid then check to see if it s alreadt taken
+
+		if (
+			this.data.username.length > 2 &&
+			this.data.username.length < 31 &&
+			validator.isAlphanumeric(this.data.username)
+		) {
+			let usernameExists = await usersCollection.findOne({ username: this.data.username });
+			if (usernameExists) {
+				this.errors.push('That username is already taken.');
+			}
+		}
+
+		// Only if email is valid then check to see if it s alreadt taken
+
+		if (validator.isEmail(this.data.email)) {
+			let emailExists = await usersCollection.findOne({ username: this.data.username });
+			if (emailExists) {
+				this.errors.push('That email is already taken.');
+			}
+		}
+		resolve();
+	});
 };
 
 User.prototype.login = function() {
-  return new Promise((resolve, reject) => {
-    this.cleanUp()
-    usersCollection.findOne({username: this.data.username}).then((attemptedUser) => {
-      if (attemptedUser && bcrypt.compareSync(this.data.password, attemptedUser.password)) {
-        resolve("Congrats!")
-      } else {
-        reject("Invalid username / password.")
-      }
-    }).catch(function() {
-      reject("Please try again later.")
-    })
-  })
+	return new Promise((resolve, reject) => {
+		this.cleanUp();
+		usersCollection
+			.findOne({ username: this.data.username })
+			.then((attemptedUser) => {
+				if (attemptedUser && bcrypt.compareSync(this.data.password, attemptedUser.password)) {
+					resolve('Congrats!');
+				} else {
+					reject('Invalid username / password.');
+				}
+			})
+			.catch(function() {
+				reject('Please try again later.');
+			});
+	});
 };
 
 User.prototype.register = function() {
-	//  Validate user data
-	this.cleanUp();
-	this.validate();
-	// Only if there are no validation errors
-	// then save the user data into database
+	return new Promise(async (resolve, reject) => {
+		//  Validate user data
+		this.cleanUp();
+		await this.validate();
+		// Only if there are no validation errors
+		// then save the user data into database
 
-	if (!this.errors.length) {
-		// has user password
-		let salt = bcrypt.genSaltSync(10);
-		this.data.password = bcrypt.hashSync(this.data.password, salt);
-		usersCollection.insertOne(this.data);
-	}
+		if (!this.errors.length) {
+			// has user password
+			let salt = bcrypt.genSaltSync(10);
+			this.data.password = bcrypt.hashSync(this.data.password, salt);
+			await usersCollection.insertOne(this.data);
+			resolve();
+		} else {
+			reject(this.errors);
+		}
+	});
 };
 
 module.exports = User;
