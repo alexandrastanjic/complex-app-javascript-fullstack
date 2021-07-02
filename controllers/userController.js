993389c611c9610455802ad7138151fd938987cc
Alexandra S <alexandrastanjic@gmail.com>
2021-07-02 14:52:25 +0300

    let users create posts

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,15 @@
-const User = require('../models/User')
+const User = require('../models/User');
+
+exports.mustBeLoggedIn = function(req, res, next) {
+  if(req.session.user) {
+    next()
+  } else {
+    req.flash("errors", "You must be logged in to perform that acction")
+    req.session.save(function() {
+      res.redirect('/')
+    })
+  }
+};
 
 exports.login = function(req, res) {
   let user = new User(req.body)
@@ -40,7 +51,7 @@ exports.register = function(req, res) {
 
 exports.home = function(req, res) {
   if (req.session.user) {
-    res.render('home-dashboard', {username: req.session.user.username, avatar: req.session.user.avatar})
+    res.render('home-dashboard')
   } else {
     res.render('home-guest', {errors: req.flash('errors'), regErrors: req.flash('regErrors')})
   }
--- router.js
+++ router.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('./controllers/userController');
+const postController = require('./controllers/postController')
 
 router.get('/', userController.home);
 router.post('/register', userController.register);
 router.post('/login', userController.login);
 router.post('/logout', userController.logout);
+
+// post realated routes
+router.get('/create-post', userController.mustBeLoggedIn, postController.viewCreateScreen);
 
 module.exports = router;
