52a949142bb62fc1549e01c9aeb93ac0733d0960
Alexandra S <alexandrastanjic@gmail.com>
2021-07-02 22:28:21 +0300

    create post model

--- router.js
+++ router.js
@@ -10,5 +10,6 @@ router.post('/logout', userController.logout);
 
 // post realated routes
 router.get('/create-post', userController.mustBeLoggedIn, postController.viewCreateScreen);
+router.post('/create-post', userController.mustBeLoggedIn, postController.create)
 
 module.exports = router;
