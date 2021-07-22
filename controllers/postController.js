fc184a8031174f9965b4997e3d320a54cbfbb4ce
Alexandra <alexandrastanjic@gmail.com>
2021-07-22 14:37:50 +0300

    add misc improvements

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,64 +1,68 @@
 const Post = require('../models/Post');
 
 exports.viewCreateScreen = function (req, res) {
-	res.render('create-post');
+  res.render('create-post');
 };
 
 exports.create = function (req, res) {
-	let post = new Post(req.body, req.session.user._id);
-	post
-		.create()
-		.then(function () {
-			res.send('New post created.');
-		})
-		.catch(function (errors) {
-			res.send(errors);
-		});
+  let post = new Post(req.body, req.session.user._id);
+  post.create().then(function (newId) {
+    req.flash("success", "New post successfully created.");
+    req.session.save(() => res.redirect(`/post/${newId}`));
+  }).catch(function (errors) {
+    errors.forEach(error => req.flash("errors", error));
+    req.session.save(() => res.redirect("/create-post"));
+  });
 };
 
 exports.viewSingle = async function (req, res) {
-	try {
-		let post = await Post.findSingleById(req.params.id, req.visitorId);
-		res.render('single-post-screen', { post: post });
-	} catch {
-		res.render('404');
-	}
+  try {
+    let post = await Post.findSingleById(req.params.id, req.visitorId);
+    res.render('single-post-screen', { post: post });
+  } catch {
+    res.render('404');
+  }
 };
 
 exports.viewEditScreen = async function (req, res) {
-	try {
-		let post = await Post.findSingleById(req.params.id);
-		res.render('edit-post', { post: post });
-	} catch {
-		res.render('404');
-	}
+  try {
+    let post = await Post.findSingleById(req.params.id, req.visitorId);
+    if (post.isVisitorOwner) {
+      res.render("edit-post", { post: post });
+    } else {
+      req.flash("errors", "You do not have permission to perform that action.");
+      req.session.save(() => res.redirect("/"));
+    }
+  } catch {
+    res.render("404");
+  }
 };
 
-exports.edit = function(req, res) {
-  let post = new Post(req.body, req.visitorId, req.params.id)
+exports.edit = function (req, res) {
+  let post = new Post(req.body, req.visitorId, req.params.id);
   post.update().then((status) => {
     // the post was successfully updated in the database
     // or user did have permission, but there were validation errors
     if (status == "success") {
       // post was updated in db
-      req.flash("success", "Post successfully updated.")
-      req.session.save(function() {
-        res.redirect(`/post/${req.params.id}/edit`)
-      })
+      req.flash("success", "Post successfully updated.");
+      req.session.save(function () {
+        res.redirect(`/post/${req.params.id}/edit`);
+      });
     } else {
-      post.errors.forEach(function(error) {
-        req.flash("errors", error)
-      })
-      req.session.save(function() {
-        res.redirect(`/post/${req.params.id}/edit`)
-      })
+      post.errors.forEach(function (error) {
+        req.flash("errors", error);
+      });
+      req.session.save(function () {
+        res.redirect(`/post/${req.params.id}/edit`);
+      });
     }
   }).catch(() => {
     // a post with the requested id doesn't exist
     // or if the current visitor is not the owner of the requested post
-    req.flash("errors", "You do not have permission to perform that action.")
-    req.session.save(function() {
-      res.redirect("/")
-    })
-  })
-}
+    req.flash("errors", "You do not have permission to perform that action.");
+    req.session.save(function () {
+      res.redirect("/");
+    });
+  });
+};
--- router.js
+++ router.js
@@ -10,24 +10,24 @@ router.post('/logout', userController.logout);
 
 // profile related routes
 router.get(
-	'/profile/:username',
-	userController.ifUserExists,
-	userController.profilePostsScreen
+  '/profile/:username',
+  userController.ifUserExists,
+  userController.profilePostsScreen
 );
 
 // post realated routes
 router.get(
-	'/create-post',
-	userController.mustBeLoggedIn,
-	postController.viewCreateScreen
+  '/create-post',
+  userController.mustBeLoggedIn,
+  postController.viewCreateScreen
 );
 router.post(
-	'/create-post',
-	userController.mustBeLoggedIn,
-	postController.create
+  '/create-post',
+  userController.mustBeLoggedIn,
+  postController.create
 );
 router.get('/post/:id', postController.viewSingle);
-router.get('/post/:id/edit', postController.viewEditScreen);
-router.post('/post/:id/edit', postController.edit);
+router.get('/post/:id/edit', userController.mustBeLoggedIn, postController.viewEditScreen);
+router.post('/post/:id/edit', userController.mustBeLoggedIn, postController.edit);
 
 module.exports = router;
